767bb2b88804e41e47e377f1d28132709cbb9bc4
valentino <v.arballo@itecriocuarto.org.ar>
2023-07-13 17:27:49 -0300

    ponderada y simple

--- ARBALLO-PR01.R
+++ ARBALLO-PR01.R
@@ -1,10 +1,34 @@
 options(echo=FALSE)
-separador <- function(ejercicio) {
-  
-  cat("<------------------ separador")
-  cat(ejercicio)
-  cat("--------------------->\n")
-  cat("\n")
+
+separador = function (ejercicio) {
+  cat("\n\n<-------------------------- Ejercicio Nº",ejercicio," -------------------------->\n\n")
 }
 
-separador()
+media_simple = function (valores) {
+  acumulador = 0
+  for (valor in valores) {
+    acumulador = acumulador + valor
+  }
+  return (acumulador/length(valores))
+}
+
+media_ponderada = function (valores, pesos) {
+  productos = c()
+  for (x in 1:length(valores)) {
+   valor = valores[x]
+   peso = pesos[x]
+    productos = c(productos, (valor*peso))
+  }
+  return (sum(productos)/sum(pesos))
+}
+
+separador(1)
+
+media_simple(c(4, 6, 8))
+
+separador(2)
+
+media_ponderada(c(0, 2, 3, 4, 3, 1, 4, 3, 3, 4, 1, 3), 5)
+
+
+
