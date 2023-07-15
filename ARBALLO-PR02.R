616fba33e5777a8268a99d068121e9cc56a7c1b9
valentino <v.arballo@itecriocuarto.org.ar>
2023-07-15 18:28:37 -0300

    desviacion tipica

---
+++ ARBALLO-PR02.R
@@ -0,0 +1,78 @@
+source("ARBALLO-PR01.R")
+
+separador = function (ejercicio) {
+  cat("\n\n<-------------------------- Ejercicio Nº",ejercicio," -------------------------->\n\n")
+}
+
+rango = function (valores) {
+  cat("rango: ", max(valores)-min(valores))
+}
+
+moda = function(x) {
+  ux = unique(x)
+  cat("moda: ", ux [which.max(tabulate(match(x, ux)))])
+}
+
+media = function (valores) {
+  acumulador = 0
+  for (valor in valores) {
+    acumulador = acumulador + valor
+  }
+  media = (acumulador/length(valores))
+  return (media)
+}
+
+mediana = function (valores) {
+  valores_ordenados = sort(valores)
+  n = length(valores_ordenados)
+  if (n %% 2 == 1) {
+    mediana = valores_ordenados[(n+1)/2]
+  } else {
+    mediana = mean(valores_ordenados[c(n/2, n/2+1)])
+  }
+  cat("valores ordenados:", valores_ordenados, "\n")
+  cat("mediana: ", mediana)
+}
+
+desviacion_media = function (valores){
+  acumulador = 0
+  for (valor in valores) {
+    acumulador = acumulador + abs(valor - media(valores)) 
+  }
+  cat("desviacion media:", acumulador/length(valores))
+}
+
+varianza = function (valores, poblacion = TRUE){
+  acumulador = 0
+  for (valor in valores) {
+    acumulador = acumulador + (valor - media(valores))^2
+  }
+  if (poblacion) {
+  cat("varianza:", acumulador/length(valores))
+  } else {
+    cat("varianza:", acumulador/(length(valores)-1))
+  }
+}
+
+desviacion_tipica = function (valores, poblacion = TRUE) {
+  acumulador = 0
+  for (valor in valores) {
+    acumulador = acumulador + (valor - media(valores))^2
+  }
+  if (poblacion) {
+    cat("desviacion tipica:", sqrt(acumulador/length(valores)))  
+  } else {
+    cat("desviacion tipica:", sqrt(acumulador/(length(valores)-1)))
+  }
+}
+
+valores4 = c(28, 29, 28, 30, 30, 29, 30, 31, 29, 29, 30, 31, 31, 31, 32, 33, 34, 34, 35, 31, 31, 32, 32, 33, 33, 31, 32, 32, 33, 33, 34)
+cat("Las temperaturas máximas en una ciudad durante el mes de enero fueron:\n", valores4, "\n")
+desviacion_media(valores4)
+rango(valores4)
+desviacion_tipica(valores4)
+varianza(valores4)
+
+# Calcula la desviación típica
+
+rm(list = ls())
