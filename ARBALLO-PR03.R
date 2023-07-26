a4a908eb3bb70043d5241ab869fa25a3900c859e
valentino <v.arballo@itecriocuarto.org.ar>
2023-07-26 20:49:31 -0300

    casi todo chau

--- ARBALLO-PR03.R
+++ ARBALLO-PR03.R
@@ -9,6 +9,7 @@ separador = function (ejercicio) {
 separador(1)
 puntuaciones = c(13, 13, 14, 15, 16, 18, 19, 20, 22, 22)
 frecuencias = c(0, 3, 1, 5, 4, 3, 1, 2, 1, 0)
+
 puntuacionesDataFrame = data.frame(puntuaciones = puntuaciones, frecuencias = frecuencias)
 ggplot(puntuacionesDataFrame, aes(x = puntuaciones, y = frecuencias)) +
   geom_point(na.rm = FALSE, stat = "identity") +
@@ -20,7 +21,48 @@ ggplot(puntuacionesDataFrame, aes(x = puntuaciones, y = frecuencias)) +
   scale_y_continuous(limits = c(0, 6), breaks = seq(0, 6, by = 1))
 
 separador(2)
+numeroEstrellas = c(1, 2, 3, 4)
+frecuencias2 = c(6, 12, 16, 4)
+
+estrellasDataFrame = data.frame(numeroEstrellas = numeroEstrellas, frecuencias2 = frecuencias2)
+
+ggplot(estrellasDataFrame, aes(x = numeroEstrellas, y = frecuencias2)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(limits = c(0, 5), breaks = seq(0, 5, y = 1)) +
+  scale_y_continuous(limits = c(0, 16), breaks = seq(0, 16, by = 1))
+
+separador(3)
+calificaciones = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+frecuencias3 = c(1, 1, 2, 3, 6, 11, 12, 7, 4, 2, 1)
+
+notasDataFrame = data.frame(calificaciones = calificaciones, frecuencias3 = frecuencias3)
+
+ggplot(notasDataFrame, aes(x = calificaciones, y = frecuencias3)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(limits = c(0, 10), breaks = seq(0, 10, y = 1)) +
+  scale_y_continuous(limits = c(0, 13), breaks = seq(0, 13, by = 1))
+ 
+
+separador(4)
+limites_inferiores4 = c(50, 60, 70, 80, 90, 100, 110)
+limites_superiores4 = c(60, 70, 80, 90, 100, 110, 120)
+frecuencias4 = c(8, 10, 16, 14, 10, 5, 2)
+
+intervalos4 = data.frame(limites_inferiores4 = limites_inferiores4,
+                         limites_superiores4 = limites_superiores4,
+                         frecuencias4 = frecuencias4)
+
+puntosMedios4 = (intervalos4$limites_inferiores4 + intervalos4$limites_superiores4) / 2
+
+empleadosDataFrame = data.frame(puntosMedios4 = puntosMedios4, frecuencias4 = frecuencias4)
+
+ggplot(empleadosDataFrame, aes(x = puntosMedios4, y = frecuencias4)) +
+  geom_bar(stat = "identity") +
+  geom_line(na.rm = FALSE, stat = "identity") +
+  scale_x_continuous(limits = c(40, 125), breaks = seq(40, 125, by = 10)) +
+  scale_y_continuous(limits = c(0, 17), breaks = seq(0, 17, by = 1)) 
 
 
-rm(list = ls())
+
+# rm(list = ls())
 
